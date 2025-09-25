c6ef92e26b85979ecea9f393e7480c1c260688ee
Eugene <legendeugene66@gmail.com>
2025-09-25 11:15:57 +0300

    first commit

---
+++ work.c
@@ -0,0 +1,17 @@
+/*
+NAME:Eugene Wafula
+REG NO:PA106/G/28760/25
+DESCRIPTION:A C program to display all units for semester 1
+*/
+
+#include <stdio.h.>
+int main() {
+    printf("SPC2101 Introduction to programming and problem solvig\n");
+    printf("SPI2101 Computer system and organization\n");
+    printf("SPM2100 Descrete mathematics\n");
+    printf("SPM2104 Mathematics for science\n");
+    printf("SSE2101 Fundamentals of systems analysis and design\n");
+    printf("UCU2101 Fundamentals of ICT\n");
+    printf("UCU2106Communication skills\n");
+    return 0;
+}   
