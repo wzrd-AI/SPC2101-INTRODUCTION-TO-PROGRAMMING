7948f61a585a14b11e9425b78bc99b16f7de5272
wzrd-AI <legendeugene66@gmail.com>
2025-10-08 13:50:56 +0300

    Add files via upload

---
+++ numbers.c
@@ -0,0 +1,16 @@
+/*
+  NAME:Eugene wafula
+  REG NO:PA106/G/28760/25
+  DESCRIPTION:For loop to print all the numbers from 100 down to 50 in descending order
+*/
+
+#include <stdio.h>
+int main() {
+    //for loop
+    int i;
+    for (i = 100; i >=50; i--) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
